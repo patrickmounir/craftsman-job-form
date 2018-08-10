726a03848e5f633f1477cbfc663d010dd6f71864
Patrick Attia <patrickmounir@gmail.com>
2018-08-10 13:08:42 +0200

    Made factory

--- database/factories/JobRequestFactory.php
+++ database/factories/JobRequestFactory.php
@@ -4,6 +4,17 @@ use Faker\Generator as Faker;
 
 $factory->define(App\JobRequest::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory(\App\User::class)->create()->id;
+        },
+        'service_id' => function () {
+            return factory(\App\Service::class)->create()->id;
+        },
+
+        'title' => $faker->word,
+        'description' => $faker->sentence,
+        'zip' => $faker->postcode,
+        'city' => $faker->city,
+        'deadline' => $faker->dateTimeBetween('+1 days', '+30 days'),
     ];
 });
